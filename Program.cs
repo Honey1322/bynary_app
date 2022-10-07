97c811457921c4eee81206a58c6c5cfa766e5d91
Valya <104560380+Honey1322@users.noreply.github.com>
2022-10-07 21:30:11 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿
 Console.WriteLine("Перевод предложений (слов) из латиницы/кириллицы в вид двоичного кода");
+Console.WriteLine("Начало программы!");
+Console.WriteLine("Введите слово/предложение");
+Console.ReadLine();
+
